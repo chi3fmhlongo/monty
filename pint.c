a865f3f59c1afd1cd8e68331b454b1973f90df91
Siyabonga Khwela <61106889+chi3fmhlongo@users.noreply.github.com>
2023-12-14 21:10:47 +0200

    Update pint.c

--- pint.c
+++ pint.c
@@ -1,10 +1,5 @@
 #include "monty.h"
-/**
- * f_pint - prints the top
- * @head: stack head
- * @counter: line_number
- * Return: no return
-*/
+
 void f_pint(stack_t **head, unsigned int counter)
 {
 	if (*head == NULL)
